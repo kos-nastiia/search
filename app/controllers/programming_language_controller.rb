03cb767a1ae9bf5c0e3013322880140024f3b6f2
kos-nastiia <kostyrka.anastasiia@gmail.com>
2024-01-05 12:58:36 +0200

    add search by name

--- app/controllers/programming_language_controller.rb
+++ app/controllers/programming_language_controller.rb
@@ -4,6 +4,19 @@ class ProgrammingLanguageController < ApplicationController
   end
 
   def search
+    @programming_languages = DATA
 
+  if params[:query].present?
+    @search_results = @programming_languages.select do |language|
+      language[:Name].downcase.include?(params[:query].downcase)
+    end
+  else
+    @search_results = @programming_languages
+  end
+
+  render turbo_stream: turbo_stream.replace("programming_language",
+    partial: "programming_language/table",
+    locals: { programming_language: @search_results }
+  )
   end
 end
